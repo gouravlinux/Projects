f1b3adacb5b81a36f53e6e449b44a4158e4a7da9
gouravlinux <2005gouravsoni@gmail.com>
2025-11-19 21:17:20 +0530

    changed email port to take value from environment variable

--- e_chayan_project/settings.py
+++ e_chayan_project/settings.py
@@ -145,7 +145,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_PORT = os.environ.get('EMAIL_PORT') 
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
